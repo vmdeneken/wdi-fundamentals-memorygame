86b6759326b3415c2995d8ee78cc7025fe1bc292
Jorge Murguia <jorgemurguia@JorgeMuuiasiMac.attlocal.net>
2016-11-22 22:57:14 -0800

    alert popup

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,12 @@
 console.log("JS file is connected to HTML! Woo!")
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+ if ( cardOne === cardTwo){
+    alert("You found a match!");
+} else {
+    alert("Sorry, try again.");
+}
